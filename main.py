ef17978280a51a96bd98874883392bd3db95dff8
Renier <slowcheetah8@gmail.com>
2017-04-28 22:00:03 +0200

    edited main

--- main.py
+++ main.py
@@ -11,38 +11,16 @@ relay_1.value(1)
 relay_2.value(1)
 last_direction = 'up'
 
-# function to drive a motor in one direction (used for testing my circuit)
-def up_on():
-    try:
-        while True:
-            last_direction = 'up'
-            relay_1.value(0)
-            print("Going up")
-            read_states()
-            sleep(0.5)
-    except KeyboardInterrupt:
-        relay_1.value(1)
-
-# function to drive motor in the opposite direction (used for testing my circuit)
-def down_on():
-    try:
-        while True:
-            last_direction = 'down'
-            relay_2.value(0)
-            print("Going Down")
-            read_states()
-            sleep(0.5)
-    except KeyboardInterrupt:
-        relay_2.value(1)	
-        
 def up():
+    global last_direction
     last_direction = 'up'
     relay_1(0)
 
 def down():
+    global last_direction
     last_direction = 'down'
     relay_2(0)
-    
+
 def read_states():
     print("R1 = ", relay_1.value(), " R2 =", relay_2.value())
 
@@ -53,20 +31,19 @@ def all_off():
 # print initial states               
 print(read_states())
 
-def run(last_direction):
+def run():
     # run in the oposite direction
     if last_direction == 'up':
         down()
     else:
         up()
-        
+
 def main():
     while True:
         #if button is pressed
         if not button.value():
             #if stopped
-            if relay_1.value() == 1 and relay_2.value() == 1:
-                run(last_direction)
+            if relay_1.value() == 0 and relay_2.value() == 0:
+                run()
             else:
                 all_off()
-        
