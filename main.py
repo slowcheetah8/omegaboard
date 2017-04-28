fc6d1763510adc1bbbbead8d3acc83b48035d8ed
Renier <slowcheetah8@gmail.com>
2017-04-28 18:48:21 +0200

    edited main

--- main.py
+++ main.py
@@ -5,44 +5,68 @@ relay_1 = Pin(12, Pin.OUT)
 relay_2 = Pin(13, Pin.OUT)
 button = Pin(0, Pin.IN)
 
+
 #Setting initial relay states
-relay_1.value(0)
-relay_2.value(0)
+relay_1.value(1)
+relay_2.value(1)
+last_direction = 'up'
 
 # function to drive a motor in one direction (used for testing my circuit)
 def up_on():
-	try:
-		while True:
-			relay_1.value(1)
-			print("Going up")
-			read_states()
-			sleep(0.5)
-	except KeyboardInterrupt:
-		relay_1.value(0)
-		
+    try:
+        while True:
+            last_direction = 'up'
+            relay_1.value(0)
+            print("Going up")
+            read_states()
+            sleep(0.5)
+    except KeyboardInterrupt:
+        relay_1.value(1)
+
 # function to drive motor in the opposite direction (used for testing my circuit)
 def down_on():
-	try:
-		while True:
-			relay_2.value(1)
-			print("Going Down")
-			read_states()
-			sleep(0.5)
-	except KeyboardInterrupt:
-		relay_2.value(0)	
-		
+    try:
+        while True:
+            last_direction = 'down'
+            relay_2.value(0)
+            print("Going Down")
+            read_states()
+            sleep(0.5)
+    except KeyboardInterrupt:
+        relay_2.value(1)	
+        
+def up():
+    last_direction = 'up'
+    relay_1(0)
+
+def down():
+    last_direction = 'down'
+    relay_2(0)
+    
 def read_states():
-	print("R1 = ", R1.value(), " R2 =", R2.value())
+    print("R1 = ", relay_1.value(), " R2 =", relay_2.value())
 
 def all_off():
-    relay_1.value(0)
-    relay_2.value(0)
+    relay_1.value(1)
+    relay_2.value(1)
 
 # print initial states               
 print(read_states())
 
-
-
-
-
-	
+def run(last_direction):
+    # run in the oposite direction
+    if last_direction == 'up':
+        down()
+    else:
+        up()
+        
+def main():
+    while True:
+        #if button is pressed
+        if not button.value():
+            #if stopped
+            if relay_1.value() == 1 and relay_2.value() == 1:
+                run(last_direction)
+            else:
+                all_off()
+        
