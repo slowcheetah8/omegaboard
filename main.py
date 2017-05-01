030097530e9789be535a02d8a287654cea661cfc
Renier <slowcheetah8@gmail.com>
2017-05-01 17:25:06 +0200

    working main.py... can now stop

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from machine import Pin
-from time import sleep
+import time
+
+
 
 relay_1 = Pin(12, Pin.OUT)
 relay_2 = Pin(13, Pin.OUT)
 button = Pin(0, Pin.IN)
 
-
-#Setting initial relay states
+#Setting initial relay states to all_off
+# the pins are inverted, so: 1 = off and 0 = on
 relay_1.value(1)
 relay_2.value(1)
 last_direction = 'up'
@@ -14,41 +16,43 @@ last_direction = 'up'
 def up():
     global last_direction
     last_direction = 'up'
+    #switches relay_1 on
     relay_1(0)
-
+    print("Going up")
+    
 def down():
     global last_direction
     last_direction = 'down'
+    #switches relay_1 on
     relay_2(0)
-
-def read_states():
-    print("R1 = ", relay_1.value(), " R2 =", relay_2.value())
-
-def all_off():
+    print("Going down")
+    
+def stay():
     relay_1.value(1)
     relay_2.value(1)
+    print("Stopped")
+    
+def read_states():
+    print("R1 = ", relay_1.value(), " R2 =", relay_2.value())
 
 # print initial states               
 print(read_states())
 
-def run():
-    # run in the oposite direction
-    if last_direction == 'up':
-        all_off()
-        sleep(1)
-        down()
-    else:
-        all_off()
-        sleep(1)
-        up()
+funcs = (up, stay, down, stay)
+index = 0
 
 def main():
+    global funcs
+    global index
     while True:
-        #if button is pressed
-        if button.value() == 0:
-            #if stopped
-            if relay_1.value() == 1 and relay_2.value() == 1:
-                run()
-            elif relay_1.value() == 0 or relay_2.value() == 0:
-                all_off()
+        if button.value() ==0:
+            if index == 4:
+                index = 0
+            funcs[index]()
+            index += 1
+            time.sleep(1)
+    
+
+            
+                
                 
